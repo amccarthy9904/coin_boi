ce60f01931a7bb06db8977f18de2da413365d474
Sage <sagersmith8@gmail.com>
2018-02-11 12:51:36 -0700

    Made gather data a function

--- src/data_grab.py
+++ src/data_grab.py
@@ -1,10 +1,16 @@
 import requests
 import json
 
-rate_map = {}
-r = requests.get("https://shapeshift.io/marketinfo")
-result = json.loads(r.text)
-for cur_map in result:
-    rate_map[cur_map[u'pair']] = cur_map[u'rate']
 
-print rate_map
+def gather_data():
+    """
+    Grabs all of the coins and organizes their exchange rates
+
+    :return: A map from coins to exchange rate
+    """
+    rate_map = {}
+    r = requests.get("https://shapeshift.io/marketinfo")
+    result = json.loads(r.text)
+    for cur_map in result:
+        rate_map[cur_map[u'pair']] = cur_map[u'rate']
+    return rate_map
